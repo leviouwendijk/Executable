18336d14831885404d8d1bb9067068fb9604dfe6
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-26 02:10:37 +0200

    pty

--- Sources/Executable/build/build.swift
+++ Sources/Executable/build/build.swift
@@ -43,11 +43,9 @@ public enum Build {
         public let buildDirComponent: String
     }
 
+    // USE PTY
     @discardableResult
     private static func runSwift(command: [String], in dir: URL) async throws -> BuildResult {
-        var opt = Shell.Options()
-        opt.cwd = dir
-
         // mirror sbm’s streamed output with light paint
         let colorables: [ColorableString] = [
             .init(selection: ["production", "debugging"], colors: [.bold]),
@@ -55,59 +53,102 @@ public enum Build {
             .init(selection: ["warning"], colors: [.yellow]),
             .init(selection: ["Build complete!"], colors: [.green])
         ]
-
-        // NEW: USING LINESTREAMER INVOCATION
         let painter: @Sendable (String) -> String = { $0.paint(colorables) }
 
         let outStreamer = LineStreamer(handle: .standardOutput, colorize: true, paint: painter)
-        let errStreamer = LineStreamer(handle: .standardError,  colorize: true, paint: painter)
 
-        opt.teeToStdout = false
-        opt.teeToStderr = false
+        var childEnv = ProcessInfo.processInfo.environment
+        childEnv["NSUnbufferedIO"] = "YES"
 
-        opt.onStdoutChunk = { chunk in
-            debugDumpChunk(label: "STDOUT", chunk: chunk)
-            Task.detached(priority: .userInitiated) { await outStreamer.ingest(chunk) }
-        }
-        opt.onStderrChunk = { chunk in
-            debugDumpChunk(label: "STDERR", chunk: chunk)
-            Task.detached(priority: .userInitiated) { await errStreamer.ingest(chunk) }
-        }
-
-        let res = try await Shell(.zsh).run(
+        let resPTY = try runPTY(
             "/usr/bin/env",
             ["swift"] + command,
-            options: opt
+            env: childEnv,
+            cwd: dir,
+            onChunk: { chunk in
+                debugDumpChunk(label: "PTY", chunk: chunk)
+                Task.detached(priority: .userInitiated) { await outStreamer.ingest(chunk) }
+            }
         )
 
-        // just causes slowdowns:
-        // let res = try await Shell(.path("/usr/bin/script")).run(
-        //     "/usr/bin/script",
-        //     [ "-q", "/dev/null", "/usr/bin/env", "swift" ] + command,
-        //     options: opt
-        // )
-
         await outStreamer.flush()
-        await errStreamer.flush()
 
-        let code = res.exitCode ?? 0
+        let code = resPTY.exitCode
         if code != 0 {
-            let out = String(data: res.stdout, encoding: .utf8) ?? ""
-            let err = res.stderrText()
-            throw BuildError.swiftFailed(exitCode: Int(code), stdout: out, stderr: err)
+            let out = String(data: resPTY.stdout, encoding: .utf8) ?? ""
+            throw BuildError.swiftFailed(exitCode: Int(code), stdout: out, stderr: "")
         }
 
         let mode: Config.Mode = command.contains { $0.lowercased() == "debug" } ? .debug : .release
-
         return BuildResult(
-            exitCode: Int32(code),
-            stdout: res.stdout,
-            stderr: res.stderr,
+            exitCode: code,
+            stdout: resPTY.stdout,
+            stderr: Data(),
             mode: mode,
             buildDirComponent: (mode == .debug ? "debug" : "release")
         )
-        // END OF NEW LINESTREAMER INVOCATION
     }
+
+    // USE LINESTREAMER
+
+    // @discardableResult
+    // private static func runSwift(command: [String], in dir: URL) async throws -> BuildResult {
+    //     var opt = Shell.Options()
+    //     opt.cwd = dir
+
+    //     // mirror sbm’s streamed output with light paint
+    //     let colorables: [ColorableString] = [
+    //         .init(selection: ["production", "debugging"], colors: [.bold]),
+    //         .init(selection: ["error"], colors: [.red]),
+    //         .init(selection: ["warning"], colors: [.yellow]),
+    //         .init(selection: ["Build complete!"], colors: [.green])
+    //     ]
+
+    //     // NEW: USING LINESTREAMER INVOCATION
+    //     let painter: @Sendable (String) -> String = { $0.paint(colorables) }
+
+    //     let outStreamer = LineStreamer(handle: .standardOutput, colorize: true, paint: painter)
+    //     let errStreamer = LineStreamer(handle: .standardError,  colorize: true, paint: painter)
+
+    //     opt.teeToStdout = false
+    //     opt.teeToStderr = false
+
+    //     opt.onStdoutChunk = { chunk in
+    //         debugDumpChunk(label: "STDOUT", chunk: chunk)
+    //         Task.detached(priority: .userInitiated) { await outStreamer.ingest(chunk) }
+    //     }
+    //     opt.onStderrChunk = { chunk in
+    //         debugDumpChunk(label: "STDERR", chunk: chunk)
+    //         Task.detached(priority: .userInitiated) { await errStreamer.ingest(chunk) }
+    //     }
+
+    //     let res = try await Shell(.zsh).run(
+    //         "/usr/bin/env",
+    //         ["swift"] + command,
+    //         options: opt
+    //     )
+
+    //     await outStreamer.flush()
+    //     await errStreamer.flush()
+
+    //     let code = res.exitCode ?? 0
+    //     if code != 0 {
+    //         let out = String(data: res.stdout, encoding: .utf8) ?? ""
+    //         let err = res.stderrText()
+    //         throw BuildError.swiftFailed(exitCode: Int(code), stdout: out, stderr: err)
+    //     }
+
+    //     let mode: Config.Mode = command.contains { $0.lowercased() == "debug" } ? .debug : .release
+
+    //     return BuildResult(
+    //         exitCode: Int32(code),
+    //         stdout: res.stdout,
+    //         stderr: res.stderr,
+    //         mode: mode,
+    //         buildDirComponent: (mode == .debug ? "debug" : "release")
+    //     )
+    //     // END OF NEW LINESTREAMER INVOCATION
+    // }
 
     // // COLORING, BUT NO STREAM
     // @discardableResult
