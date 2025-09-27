a15a5e6fe8177e0e8aa6c862ed617c32c9154dbc
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-27 02:19:45 +0200

    change in wording of print

--- Sources/Executable/build/build.swift
+++ Sources/Executable/build/build.swift
@@ -129,7 +129,7 @@ public enum Build {
             let v_compiled = compl_cfg.version
             if v_compiled == v_release { 
                 print("compiled == release")
-                print("no ".ansi(.brightBlack) + "compiled.pkl".ansi(.brightBlack, .bold) + " will be written".ansi(.brightBlack))
+                print("no ".ansi(.brightBlack) + "compiled.pkl".ansi(.brightBlack, .bold) + " written".ansi(.brightBlack))
                 return 
             }
         }
